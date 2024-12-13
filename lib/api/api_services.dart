1778eca8f6e9fdbe55b463d85cdef37e4ea1fa35
Haneen Salama <shaneen934@gmail.com>
2024-12-13 19:05:21 +0200

    initialize project

--- lib/api/api_services.dart
+++ lib/api/api_services.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:movies/api/api_constants.dart';
-import 'package:movies/api/responses/new_release_response.dart';
-import 'package:movies/api/responses/popular_response.dart';
-import 'package:movies/api/responses/recommended_response.dart';
-import 'package:movies/movie_items/movies_info/new_releases.dart';
+import 'package:movies_app/api/api_constants.dart';
+import 'package:movies_app/api/responses/new_release_response.dart';
+import 'package:movies_app/api/responses/popular_response.dart';
+import 'package:movies_app/api/responses/recommended_response.dart';
 
-class APIServices{
-  static Future<PopularResponse> getPopular(String categoryId)async{
-    final uri = Uri.https(APIConstants.baseURL,APIConstants.popularEndpoint,{
+class APIServices {
+  static Future<PopularResponse> getPopular(String categoryId) async {
+    final uri = Uri.https(APIConstants.baseURL, APIConstants.popularEndpoint, {
       'apiKey': APIConstants.apiKey,
     });
     final response = await http.get(uri);
@@ -16,8 +15,9 @@ class APIServices{
     return PopularResponse.fromJson(json);
   }
 
-  static Future<NewReleaseResponse> getNewRelease(String sourceId)async{
-    final uri = Uri.https(APIConstants.baseURL,APIConstants.newReleaseEndpoint,{
+  static Future<NewReleaseResponse> getNewRelease(String sourceId) async {
+    final uri =
+        Uri.https(APIConstants.baseURL, APIConstants.newReleaseEndpoint, {
       'apiKey': APIConstants.apiKey,
     });
     final response = await http.get(uri);
@@ -25,8 +25,9 @@ class APIServices{
     return NewReleaseResponse.fromJson(json);
   }
 
-  static Future<RecommendedResponse> getRecommended(String sourceId)async{
-    final uri = Uri.https(APIConstants.baseURL,APIConstants.recommendedEndpoint,{
+  static Future<RecommendedResponse> getRecommended(String sourceId) async {
+    final uri =
+        Uri.https(APIConstants.baseURL, APIConstants.recommendedEndpoint, {
       'apiKey': APIConstants.apiKey,
     });
     final response = await http.get(uri);
